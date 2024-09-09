97f8bad2734639704a40492e22f6375c63c4f641
wongsakron kongkamud <wongsakronkongkamud@gmail.com>
2024-09-09 16:44:47 +0700

    Release 1.0 2nd commit frontend

---
+++ frontend/angular-ecommerce/src/app/compoent/product-list/product-list.component.ts
@@ -0,0 +1,32 @@
+import { Component, OnInit } from '@angular/core';
+import { ProductService } from './../../services/product.service';
+import { Product } from '../../common/product';
+
+@Component({
+  selector: 'app-product-list',
+  templateUrl: './product-list-table.component.html',
+  styleUrl: './product-list.component.css'
+})
+export class ProductListComponent implements OnInit {
+
+    products: Product[] = [];
+    // inject productservice
+    constructor(private productService: ProductService) {
+
+    }
+
+    // similar to @PostConstruct
+    ngOnInit(): void {
+      this.listProducts();
+    }
+
+    // method is invoked once you "subscribe" อซิงโคนัส
+    listProducts() {
+      this.productService.getProductList().subscribe(
+        data => {
+          this.products = data
+          // assign results to the Product array
+        }
+      )
+    }
+}
---
+++ frontend/angular-ecommerce/src/app/services/product.service.ts
@@ -0,0 +1,31 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { Product } from '../common/product';
+import { map } from 'rxjs/operators'
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ProductService {
+
+  private baseUrl = 'http://localhost:8080/api/products'
+
+  constructor(private httpClient: HttpClient) { }
+
+  // return an observable  MAP the Json data from Spring Data Rest to
+  // Product array
+  getProductList(): Observable<Product[]> {
+    return this.httpClient.get<GetResponse>(this.baseUrl).pipe(
+      map(response => response._embedded.products)
+    );
+  }
+}
+
+// Unwraps the JSON from Spring Data REST _embedded entry
+// access the array of product
+interface GetResponse {
+  _embedded: {
+    products: Product[];
+  }
+}
